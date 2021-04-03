da3bd9db7cc91c6b328a7e3e0c1aa31e4fecd44a
Ishita <ishita_arora_@outlook.com>
2021-04-03 23:33:30 +0530

    code optimised

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fitnessApp/screens/homePage.dart';
+import 'package:fitnessApp/database/database.dart';
+import 'package:fitnessApp/tempPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'login/loginPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +20,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    checkSignIn().then((value) => null);
     return MaterialApp(
       title: 'Fitness App',
       debugShowCheckedModeBanner: false,
@@ -24,7 +28,61 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       // theme: MyTheme.lightTheme(context),
       // darkTheme: Mytheme.darkTheme(context),
-      home: HomePage(),
+      home: CheckUserStatus(),
     );
   }
+}
+
+class CheckUserStatus extends StatefulWidget {
+  CheckUserStatus({Key key}) : super(key: key);
+
+  @override
+  _CheckUserStatusState createState() => _CheckUserStatusState();
+}
+
+class _CheckUserStatusState extends State<CheckUserStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: checkSignIn(),
+        builder: (context, snapshot) {
+          var page;
+          if (snapshot.hasData) {
+            switch (snapshot.data) {
+              case "login":
+                {
+                  page = LoginPage();
+                  break;
+                }
+              case "home":
+                {
+                  page = TempPage();
+                  break;
+                }
+              case "initial":
+                {
+                  // page = InitialDetailPage();
+                  break;
+                }
+            }
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: page,
+            );
+          } else {
+            return Container(
+              color: Colors.white,
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+        });
+  }
+}
+
+Future<String> checkSignIn() async {
+  await DatabaseService().getCR();
+  if (FirebaseAuth.instance.currentUser == null)
+    return "login";
+  else
+    return "home";
 }
